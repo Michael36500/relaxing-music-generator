d698d098952dfa3dc6b00290f323b615c6c6022d
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-08-19 10:36:29 +0200

    Relaxing music + started dark melodies

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 from pydub import AudioSegment
 import random as rn
+from tqdm import tqdm
 
-sandman = AudioSegment.from_mp3("sandman.mp3")
-marimba = AudioSegment.from_mp3("bright_marimba.mp3")
-string = AudioSegment.from_mp3("jarble_tweeze_string.mp3")
-bell = AudioSegment.from_mp3("outland_bell.mp3")
-choir = AudioSegment.from_mp3("spacious_choir.mp3")
+bass = AudioSegment.from_wav("bass.wav")
+DRdetonate = AudioSegment.from_wav("DRdetonate.wav")
+DRmaster = AudioSegment.from_wav("DRmaster.wav")
+hihat = AudioSegment.from_wav("hihat.wav")
+keys = AudioSegment.from_wav("keys.wav")
+pad = AudioSegment.from_wav("pad.wav")
+piano = AudioSegment.from_wav("piano.wav")
+print("empty")
+output = AudioSegment.from_wav("empty.wav")
 
-for _ in range(10):
-    cis_sa = rn.randint(0, 4)
-    cis_ma = rn.randint(0, 4)
-    cis_st = rn.randint(0, 4)
-    cis_be = rn.randint(0, 4)
+length_one_clip = 23000
+length_total = 2 * 60 * 1000
 
-    if cis_sa
-    
+loops = length_total // length_one_clip + 1
+
+for lp in tqdm(range(loops)):
+    bass = rn.randint(0,1)
+    keys = rn.randint(0,1)
+    pad = rn.randint(0,1)
+    piano = rn.randint(0,1)
+
+    drums = rn.randint(0,2)
+
+    if bass == 1:
+        output = output.overlay(bass, position=lp*length_one_clip)
+    if keys == 1:
+        output = output.overlay(keys, position=lp*length_one_clip)
+    if pad == 1:
+        output = output.overlay(pad, position=lp*length_one_clip)
+    if piano == 1:
+        output = output.overlay(piano, position=lp*length_one_clip)
+
+    if drums == 0:
+        output = output.overlay(DRdetonate, position=lp*length_one_clip)
+    if drums == 1:
+        output = output.overlay(DRmaster, position=lp*length_one_clip)
+    if drums == 2:
+        output = output.overlay(hihat, position=lp*length_one_clip)
+        
 
 
-# mix sound2 with sound1, starting at 5000ms into sound1)
-output = sound1.overlay(sound2, position=5000)
 
 # save the result
 output.export("mixed_sounds.mp3", format="mp3")
